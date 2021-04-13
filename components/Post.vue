0de3b5fb42d379046a66030c8e24f135f99015b1
Azael Contramaestre <galaxyhhd@hotmail.com>
2021-04-13 19:53:33 -0300

    good

---
+++ components/Post.vue
@@ -0,0 +1,39 @@
+<template>
+
+<nuxt-link :to="'/posts/' + id">
+    <div class="post">
+        <p>{{id}}. {{name}}</p>
+    </div>
+</nuxt-link>
+    
+</template>
+
+<script>
+export default {
+        name: 'Post',
+        props: ['id', 'name', 'content']
+}
+</script>
+
+<style>
+    .post {
+        padding: 1rem;
+        border: 1px solid whitesmoke;
+        margin: 1rem 0;
+    }
+
+    .post:hover {
+        background: #2d3436;
+        border: 1px solid #6c5ce7;
+        cursor: pointer;
+    }
+    
+    a {
+        text-decoration: none;
+        color: whitesmoke
+    }
+
+    a:hover {
+        color: #6c5ce7;
+    }
+</style>
--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,25 @@
 <template>
-    <h1>About Us</h1>
+    <div>
+        <h1>About us</h1>
+    </div>
 </template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'About Us | Index',
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content : "Website about jamom"
+        }
+      ],
+      htmlAttrs: {
+        lang: 'eng'
+      }
+    }
+  },
+}
+</script>
